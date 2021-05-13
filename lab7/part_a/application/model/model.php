46b85fcbbe930c407099b722786c580722000714
eb513 <84129901+eb513@users.noreply.github.com>
2021-05-13 12:33:08 +0100

    Add files via upload

---
+++ lab7/part_a/application/model/model.php
@@ -0,0 +1,29 @@
+<?php
+
+class Model{
+
+    public function model3D_info(){
+        return array(
+            'model1' => 'Coke Can 3D Image 1',
+            'image3D_1' => 'coke_1',
+
+            'model2' => 'Coke Can 3D Image 2',
+            'image3D_2' => 'coke_2',
+
+            'model3' => 'Sprite Bottle 3D Image 1',
+            'image3D_3' => 'sprite_1',
+
+            'model4' => 'Sprite Bottle 3D Image 2',
+            'image3D_4' => 'sprite_2',
+
+            'model5' => 'Fanta Cup 3D Image 1',
+            'image3D_5' => 'fanta_1',
+
+            'model6' => 'Fanta Cup 3D Image 2',
+            'image3D_6' => 'fanta_2'
+        );
+    }
+
+}
+
+?>
